c02c051a8d4ec32eb78fe658c16443226beb3a2f
Jester <justjestervz@gmail.com>
2022-06-14 11:44:06 +0300

    add isolate

--- lib/http.dart
+++ lib/http.dart
@@ -6,7 +6,8 @@ import 'package:sms_reader/model/log.dart';
 import 'package:sms_reader/model/settings_model.dart';
 import 'package:telephony/telephony.dart';
 import 'dart:convert';
-class HttpClient{
+
+class HttpClient {
   final Dio _apiClient = _getDio(baseUrl: null);
   static Dio _getDio({String? baseUrl}) {
     return Dio(BaseOptions(
@@ -16,32 +17,67 @@ class HttpClient{
     ));
   }
 
-  Future<Object>? sendToServer(SmsMessage message) async{
+  Future<Object>? sendToServer(SmsMessage message) async {
     Log log = locator<Log>();
-    log.messages += ("${DateTime.now()} message address: ${message.address} \n");
+    log.messages +=
+        ("${DateTime.now()} message address: ${message.address} \n");
     log.messages += ("${DateTime.now()} message body: ${message.body} \n");
     SharedPreferences preferences = await SharedPreferences.getInstance();
     String? serverLink = preferences.getString('serverLink');
-    
+
     String url = serverLink ?? '';
     log.messages += ("${DateTime.now()} serverLink: $url \n");
     var formData = FormData.fromMap({
       'my_number': preferences.getString('myNumber') ?? '',
       'sender_number': message.address,
-      'card': message.address == '900' ? preferences.getString('sber') : message.address == 'tinkoff' ? preferences.getString('tinkoff') : '',
+      'card': message.address == '900'
+          ? preferences.getString('sber')
+          : message.address == 'Tinkoff'
+              ? preferences.getString('tinkoff')
+              : 'other number',
       'text': message.body
     });
     var responce;
-    for(int i = 0; i < preferences.getStringList('numbersList')!.length; i++){
-      if(message.address == preferences.getStringList('numbersList')![i]){
-        log.messages += ("${DateTime.now()} number: ${preferences.getStringList('numbersList')![i]} \n");
+    for (int i = 0; i < preferences.getStringList('numbersList')!.length; i++) {
+      if (message.address == preferences.getStringList('numbersList')![i]) {
+        log.messages +=
+            ("${DateTime.now()} number: ${preferences.getStringList('numbersList')![i]} \n");
         responce = await _apiClient.post(url, data: formData);
         break;
       }
     }
     log.messages += ("${DateTime.now()} responce: $responce \n");
-    if(responce != null && responce.statusCode == 200){
+    log.messages += ("${DateTime.now()} responce.body: ${responce.data} \n");
+    if (responce != null && responce.statusCode == 200) {
       return log;
+    } else {
+      throw Exception(responce);
+    }
+  }
+
+  Future<Object> sendToServerBack(
+      SmsMessage message, SharedPreferences preferences) async {
+    String? serverLink = preferences.getString('serverLink');
+    String url = serverLink ?? '';
+    var formData = FormData.fromMap({
+      'my_number': preferences.getString('myNumber') ?? '',
+      'sender_number': message.address,
+      'card': message.address == '900'
+          ? preferences.getString('sber')
+          : message.address == 'Tinkoff'
+              ? preferences.getString('tinkoff')
+              : 'other number',
+      'text': message.body
+    });
+    var responce;
+    for (int i = 0; i < preferences.getStringList('numbersList')!.length; i++) {
+      if (message.address == preferences.getStringList('numbersList')![i]) {
+        responce = await _apiClient.post(url, data: formData);
+        break;
+      }
+    }
+    if (responce != null && responce.statusCode == 200) {
+      return responce.data;
     } else {
       throw Exception(responce);
     }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
 import 'dart:isolate';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sms_reader/DI/locator.dart';
 import 'package:sms_reader/bloc/main_bloc.dart';
+import 'package:sms_reader/http.dart';
 import 'package:sms_reader/pages/main_page.dart';
 import 'package:telephony/telephony.dart';
 
-onBackgroundMessage(SmsMessage message) {
-  setup();
-  locator.get<MainBloc>().sendToServer(message);
+onBackgroundMessage(SmsMessage message) async {
+  createIsolate(message);
+}
+
+void createIsolate(SmsMessage message) async {
+  ReceivePort receivePort = ReceivePort();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  Isolate newIsolate;
+  newIsolate = await Isolate.spawn(
+      sendToServerOnBack, [receivePort.sendPort, message, preferences]);
+  receivePort.close();
+  newIsolate.kill();
+}
+
+void sendToServerOnBack(List<Object> args) async {
+  SendPort sendPort = args[0] as SendPort;
+  SmsMessage message = args[1] as SmsMessage;
+  SharedPreferences preferences = args[2] as SharedPreferences;
+  HttpClient client = HttpClient();
+  var result = await client.sendToServerBack(message, preferences);
+  sendPort.send(result);
 }
 
 void main() {
